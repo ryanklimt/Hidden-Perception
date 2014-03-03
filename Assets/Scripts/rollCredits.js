c1cd7345ede076c195f5340eff3b53a7ef45853c
Ryan Klimt <ryanklimt@bitbucket.org>
2014-03-02 23:39:42 -0500

    Added sources to final

--- Assets/Scripts/rollCredits.js
+++ Assets/Scripts/rollCredits.js
@@ -8,6 +8,12 @@ function Start () {
     creds += "Thanks for playing!\n";
     creds += "Created by Ryan Klimt\n";
     creds += "Game Jam 2014\n";
+    creds += "\nSources:\n";
+    creds += "http://www.dafont.com/\n";
+    creds += "https://soundcloud.com/culley905\n";
+    creds += "https://www.dropbox.com/s/yojhw46nr4q9548/clynos%20-%20ancient%20heroes.wav\n";
+    creds += "https://mega.co.nz/#!z5RhxQIS!QyboyCYP7eGUi5hNFIZ40kgbTR4HDnW396kv837ECnY\n";
+    creds += "https://www.youtube.com/watch?v=NueLZC2XWXU&list=RDtNOsY7Sv70U\n";    
     tc.text= creds;
 	crawling = true;
 }
@@ -16,7 +22,7 @@ function Update (){
     if (!crawling)
         return;
     transform.Translate(Vector3.up * Time.deltaTime * .1);
-    if(Input.GetButtonDown("Escape") || gameObject.transform.position.y > 2) {
+    if(Input.GetButtonDown("Escape") || gameObject.transform.position.y > 3) {
     	Application.LoadLevel("MainMenu");
     }
 }
